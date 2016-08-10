f79253ee6bd3c422b5729c3d3b9cb34e294cda6f
huangbaoyu <huang_baoyu@163.com>
2016-08-10 18:03:44 +0800

    修改

--- Blog/article/urls.py
+++ Blog/article/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.home),
     url(r'^detail/(?P<id>\d+)/$',views.detail,name='detail'),
     url(r'^test/$',views.test),
-    url(r'^home/$',views.home)
+    url(r'^home/$',views.home),
+    url(r'^add/$',views.add,name='add')
 ]
--- Blog/article/views.py
+++ Blog/article/views.py
@@ -4,8 +4,10 @@ from datetime import datetime
 # Create your views here.
 
 from django.http import  HttpResponse
+from tools.forms import Addform
 
 def home(request):
+
     post_list = Article.objects.all()
     return render(request, 'home.html', {'post_list': post_list})
 
@@ -15,4 +17,30 @@ def detail(request,id):
 
 
 def test(request):
-    return render(request,'test.html',{'current_time':datetime.now()})
+    if request.method == 'POST':
+        form = Addform(request.POST)
+        if form.is_valid():
+            a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
+            return HttpResponse(str(int(a) + int(b)))
+    else:
+        form = Addform()
+
+    return render(request, 'test.html', {'form': form})
+
+def add(request):
+    #表单提交
+    # a = request.GET.get('a',0)
+    # b = request.GET.get('b',0)
+    # c = int(a)+int(b)
+    #Django的forms提交
+    if request.method == 'POST':
+        form = Addform(request.POST)
+        if form.is_valid():
+            a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
+            return HttpResponse(str(int(a) + int(b)))
+    else:
+        form = Addform()
+
+    return render(request, 'test.html', {'form': form})
